54683c5376c8cf69cb4c0bfa670cecaf4d15f6b7
John Kuan <johnkuan@burpple.com>
2018-12-26 20:35:45 +0800

    commented out "/" routes for apk to work fix issue on MainActivity.kt onCreate add .travis.yml file
    Signed-off-by: John Kuan <johnkuan@burpple.com>

--- android/app/src/main/kotlin/io/dotdynamics/flutterappplayground/MainActivity.kt
+++ android/app/src/main/kotlin/io/dotdynamics/flutterappplayground/MainActivity.kt
@@ -1,11 +1,14 @@
 package io.dotdynamics.flutterappplayground
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity : FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
         GeneratedPluginRegistrant.registerWith(this)
     }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,13 @@ import 'package:flutter/material.dart';
 
 import './ui/landing_page.dart';
 import './ui/login_page.dart';
-import './ui/main_page.dart';
 
 void main() {
   runApp(new MaterialApp(
     home: new LandingPage(),
     routes: <String, WidgetBuilder>{
       "/login": (BuildContext context) => new LoginPage(),
-      "/": (BuildContext context) => new MainPage(),
+//      "/": (BuildContext context) => new MainPage(),
     },
   ));
 
